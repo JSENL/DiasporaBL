0836a89367113ce8bb51933c265ef479c5174f35
Jasen <jasenlambright@gmail.com>
2018-12-21 13:05:37 -0500

    Updated diaspora pictures need to add database and map

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-const express = require('express')
-const app = express()
+const product = require('./routes/product.route'); // Imports routes for the products
+const express = require('express');
 const bodyParser = require('body-parser');
-
+const mysql = require('mysql');
+const app = express();
 app.set("view engine", "ejs");
+const mongoose = require('mongoose');
+var dev_db_url = 'mongodb://someuser:abcd1234@ds123619.mlab.com:23619/productstutorial';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+
+
+const morgan = require('morgan');
+const mongo = require('mongodb');
+
+// app.get('/events' (req, res) => {
+// //     { name: "SmartBear Software", image: "https://smartbear.com/SmartBear/media/images/smartbear-color-logo-s.png" },
+// // { name: "Wayfair", image: "https://d2xsegqaa8s978.cloudfront.net/wayfair_0.0.4_staging/assets/logo.png" },
+// // { name: "Akamai Technologies", image: "https://www.akamai.com/us/en/multimedia/images/logo/akamai-logo.png" }
+// // res.render('events')
+// // })
+    
 app.get('/', (req, res) => {
     res.render('landing');
 });
